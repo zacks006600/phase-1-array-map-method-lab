21a096981ac89d69c681aa70814a4d1ece1e78f9
zakariaahmad <kimo.ahmad2009@gmail.com>
2022-08-12 18:38:14 +0300

    Done

--- index.js
+++ index.js
@@ -12,5 +12,9 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(sentence => {
+    return sentence.split(" ").map(word => {
+      return word[0].toUpperCase() + word.slice(1);
+    }).join(" ");
+  })
 }
